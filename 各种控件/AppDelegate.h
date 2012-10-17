4d4f9c3ba0172dd572058ab9b9c05af0892b6ebd
Ibokan <ibokan@5-27.local>
2012-10-17 10:57:32 +0800

    Initial Commit

---
+++ 各种控件/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  各种控件
+//
+//  Created by Ibokan on 12-10-17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 各种控件/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  各种控件
+//
+//  Created by Ibokan on 12-10-17.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
